071f3dd36e1bef4f44543e1b73042d83c1cedd46
JArias <jarias@wawand.co>
2021-09-25 15:28:55 -0500

    add handler and actions

---
+++ middleware/handler.go
@@ -0,0 +1,172 @@
+package middleware
+
+import (
+	"encoding/json" // package to encode and decode the json into struct and vice versa
+	"fmt"
+	"go-postgres/models" // models package where User schema is defined
+	"log"
+	"net/http" // used to access the request and response object of the api
+	// used to read the environment variable
+	"strconv" // package used to covert string into int type
+
+	"github.com/gorilla/mux" // used to get the params from the route
+
+	// package used to read the .env file
+	_ "github.com/lib/pq" // postgres golang driver
+)
+
+// response format
+type response struct {
+	ID      int64  `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+// CreateUser create a user in the postgres db
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	// set the header to content type x-www-form-urlencoded
+	// Allow all origin to handle cors issue
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// create an empty user of type models.User
+	var user models.User
+
+	// decode the json request to user
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	// call insert user function and pass the user
+	insertID := insertUser(user)
+
+	// format a response object
+	res := response{
+		ID:      insertID,
+		Message: "User created successfully",
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
+
+// GetUser will return a single user by its id
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// get the userid from the request params, key is "id"
+	params := mux.Vars(r)
+
+	// convert the id type from string to int
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int.  %v", err)
+	}
+
+	// call the getUser function with user id to retrieve a single user
+	user, err := getUser(int64(id))
+
+	if err != nil {
+		log.Fatalf("Unable to get user. %v", err)
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(user)
+}
+
+// GetAllUser will return all the users
+func GetAllUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// get all the users in the db
+	users, err := getAllUsers()
+
+	if err != nil {
+		log.Fatalf("Unable to get all user. %v", err)
+	}
+
+	// send all the users as response
+	json.NewEncoder(w).Encode(users)
+}
+
+// UpdateUser update user's detail in the postgres db
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// get the userid from the request params, key is "id"
+	params := mux.Vars(r)
+
+	// convert the id type from string to int
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int.  %v", err)
+	}
+
+	// create an empty user of type models.User
+	var user models.User
+
+	// decode the json request to user
+	err = json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	// call update user to update the user
+	updatedRows := updateUser(int64(id), user)
+
+	// format the message string
+	msg := fmt.Sprintf("User updated successfully. Total rows/record affected %v", updatedRows)
+
+	// format the response message
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
+
+// DeleteUser delete user's detail in the postgres db
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// get the userid from the request params, key is "id"
+	params := mux.Vars(r)
+
+	// convert the id in string to int
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int.  %v", err)
+	}
+
+	// call the deleteUser, convert the int to int64
+	deletedRows := deleteUser(int64(id))
+
+	// format the message string
+	msg := fmt.Sprintf("User updated successfully. Total rows/record affected %v", deletedRows)
+
+	// format the response message
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
